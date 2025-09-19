dc0cb18d4a948b66bd8863af54bc2c9c063432cb
Rozpring <s24a1070@bene.fit.ac.jp>
2025-09-19 20:17:16 +0900

    commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,6 +21,9 @@
                     <x-nav-link :href="route('tweets.create')" :active="request()->routeIs('tweets.create')">
                         {{ __('Tweet作成') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('tweets.search')" :active="request()->routeIs('tweets.search')">
+                        {{ __('Tweet検索') }}
+                    </x-nav-link>
                 </div>
             </div>
 
